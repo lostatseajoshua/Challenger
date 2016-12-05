a19295eb7a12b4697f4901af63767ddc8a64d8a0
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-04 19:45:46 -0700

    Add tests for api routes

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /**
-    Passing arrow functions (“lambdas”) to Mocha is discouraged.
-    If you do not need to use Mocha’s context, lambdas should work.
-    https://mochajs.org/#arrow-functions
+Passing arrow functions (“lambdas”) to Mocha is discouraged.
+If you do not need to use Mocha’s context, lambdas should work.
+https://mochajs.org/#arrow-functions
 */
 
 /**
@@ -58,6 +58,58 @@ describe('Express App', () => {
             request(app)
             .get('/api/')
             .expect(200, done);
+        });
+
+        describe('#challenge routes', () => {
+            it('challenges base path should return 200', (done) => {
+                request(app)
+                .get('/api/challenges')
+                .expect(200, done);
+            });
+
+            it('post to challenge should return 201', (done) => {
+                request(app)
+                .post('/api/challenges')
+                .expect(201, done);
+            });
+
+            it('update to challenges should return 204', (done) => {
+                request(app)
+                .put('/api/challenges/0')
+                .expect(204, done);
+            });
+
+            it('delete a challenge should return 204', (done) => {
+                request(app)
+                .delete('/api/challenges/0')
+                .expect(204, done);
+            });
+        });
+
+        describe('#team routes', () => {
+            it('teams base path should return 200', (done) => {
+                request(app)
+                .get('/api/teams')
+                .expect(200, done);
+            });
+
+            it('post to teams should return 201', (done) => {
+                request(app)
+                .post('/api/teams')
+                .expect(201, done);
+            });
+
+            it('update to teams should return 204', (done) => {
+                request(app)
+                .put('/api/teams/0')
+                .expect(204, done);
+            });
+
+            it('delete a team should return 204', (done) => {
+                request(app)
+                .delete('/api/teams/0')
+                .expect(204, done);
+            });
         });
     });
 });
