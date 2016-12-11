54ab149de00365e56b7075b95aa510588cde9deb
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-11 16:04:32 -0700

    Create player model

---
+++ models/Player.js
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies.
+ */
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema
+// create a schema
+const playerSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true, minlength: 1 },
+  team: Schema.Types.ObjectId,
+  created_at: Date,
+});
+
+// create a model using schema
+const Player = mongoose.model('Player', playerSchema);
+
+// on every save, add the date
+playerSchema.pre('save', (next) => {
+  if (!this.created_at) {
+      this.created_at = new Date();
+  }
+  next();
+});
+
+// make this available to our users in our Node applications
+module.exports = Player;
