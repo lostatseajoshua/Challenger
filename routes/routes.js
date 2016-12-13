4416e284d099ce8711c89e0a9f63456d7d084218
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-12 17:50:57 -0700

    Route API calls to api controller

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@
 /**
 * Module dependencies.
 */
-const express  = require('express');
-const router   = express.Router();
-const api      = require('../controllers/api');
+const express   = require('express');
+const router    = express.Router();
+const api       = require('../controllers/api');
 
 /**
 * Primary app routes.
@@ -22,31 +22,69 @@ router.get('/api/', (req, res) => {
 });
 
 /**
-* Get and post to challenges.
+* Post to games.
 */
-router.route('/api/challenges')
-.get(api.getChallenges)
-.post(api.postChallenge);
+router.route('/api/games')
+    .get(api.getGames);
 
 /**
-*  Put and delete a challenge by id.
+* Post to games.
 */
-router.route('/api/challenges/:challengeId')
-.put(api.putChallenge)
-.delete(api.deleteChallenge);
+router.route('/api/games')
+    .post(api.postGame);
+
+/**
+*  Delete a game by id.
+*/
+router.route('/api/games/:gameId')
+    .delete(api.deleteGame);
+
+/**
+*  Add a team to a game by id.
+*/
+router.route('/api/games/:gameId/addTeam')
+    .put(api.addTeam);
+
+/**
+*  Start a game by id.
+*/
+router.route('/api/games/:gameId/start')
+    .put(api.startGame);
+
+/**
+*  Finish a game by id.
+*/
+router.route('/api/games/:gameId/finish')
+    .put(api.finishGame);
+
+router.route('/api/games/:gameId/score')
+    .put(api.gameScore);
 
 /**
 * Get and post to teams.
 */
 router.route('/api/teams')
-.get(api.getTeams)
-.post(api.postTeam);
+    .get(api.getTeams)
+    .post(api.postTeam);
 
 /**
 * Put and delete a team by id.
 */
 router.route('/api/teams/:teamId')
-.put(api.putTeam)
-.delete(api.deleteTeam);
+    .put(api.putTeam)
+    .delete(api.deleteTeam);
+
+/**
+* Get and post to players
+*/
+router.route('/api/players')
+    .get(api.getPlayers)
+    .post(api.postPlayer);
+
+/**
+* Add player to team by player id
+*/
+router.route('/api/players/:playerId/addTeam')
+    .put(api.putPlayerTeam);
 
 module.exports = router;
