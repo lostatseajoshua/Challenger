b4564be8b12c5f807c697cfc4ce806cb99608a04
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-12 17:58:54 -0700

    Add PUT to add teams to game

--- controllers/api.js
+++ controllers/api.js
@@ -77,6 +77,77 @@ exports.postGame = function game(req, res) {
         });
     }
 }
+
+/**
+* PUT /api/games/:gameId/addTeam
+* Add teams to a game
+*/
+exports.addTeam = function game(req, res) {
+    const gameId = req.params.gameId;
+
+    if (!mongoose.Types.ObjectId.isValid(gameId)) {
+        res.status(403).send("Invalid player id");
+        return;
+    }
+
+    Game.findById(gameId, (err, game) => {
+        if (err) {
+            res.status(403).send(`${err}`);
+            return;
+        }
+
+        if (!game) {
+            res.status(403).send('No game found');
+            return;
+        }
+
+        if (game.started_at) {
+            res.status(403).send('Game has started cannot add a team');
+            return;
+        }
+
+        const teamIds = req.body.teams;
+
+        if (!teamIds && teamIds.length <= 0) {
+            res.status(403).send('body empty');
+            return;
+        }
+
+        var tasksToGo = teamIds.length;
+
+        teamIds.forEach(function (id) {
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                if (--tasksToGo === 0) {
+                    game.save((err, game) => {
+                        if (err) {
+                            res.status(403).send(`${err}`);
+                            return;
+                        }
+                        res.status(201).send(game);
+                    });
+                }
+                return;
+            }
+
+            Team.findById(id, (err, team) => {
+                if (!err && team) {
+                    // if (!game.teams.includes(id)) {
+                    game.teams.push(id);
+                    // }
+                }
+
+                if (--tasksToGo === 0) {
+                    game.save((err, game) => {
+                        if (err) {
+                            res.status(403).send(`${err}`);
+                            return;
+                        }
+                        res.status(201).send(game);
+                    });
+                }
+            });
+        });
+    });
 }
 
 /**
