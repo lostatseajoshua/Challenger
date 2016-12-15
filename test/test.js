f67ef3cdf702789ff697b0cdc7a2dc8ad4b67041
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 13:33:33 -0700

    Add test for Game model

--- test/test.js
+++ test/test.js
@@ -10,6 +10,7 @@ https://mochajs.org/#arrow-functions
 const assert    = require('assert');
 const request   = require('supertest');
 const Team      = require('../models/Team');
+const Game      = require('../models/Game');
 
 /**
 * App route test.
@@ -21,7 +22,7 @@ describe('Express App', () => {
     * Error route test.
     */
     describe('#error routes', () => {
-        it('should return 404 on unknown routes', (done) => {
+        it('Should return 404 on unknown routes', (done) => {
             request(app)
             .get('/foo/bar')
             .expect(404, done);
@@ -106,16 +107,16 @@ describe('Express App', () => {
 /**
 * Model test.
 */
-
 describe('Model test', () => {
     describe('#team tests', () => {
         var testTeamId;
-
+        var testTeam;
+        
         before('Before add the test team', (done) => {
             const newTeam = new Team({ name: 'Testing team 12345' });
-            newTeam.save((err, newTeam) => {
-                if (newTeam) {
-                    testTeamId = newTeam._id;
+            newTeam.save((err, team) => {
+                if (team) {
+                    testTeamId = team._id;
                 }
                 done(err);
             });
@@ -123,15 +124,72 @@ describe('Model test', () => {
 
         after('After remove the test team', (done) => {
             Team.findByIdAndRemove(testTeamId, (err, team) => {
+                assert(team);
                 done(err);
             });
         });
 
         it('Should retrieve a team by the name Testing team 12345', (done) => {
             Team.findOne({ name: 'Testing team 12345' }, (err, team) => {
-                assert(team.created_at);
+                assert(team);
+                testTeam = team;
                 done(err);
             });
         });
-    })
+
+
+        it('Should check team\'s created_at property', (done) => {
+            assert(testTeam.created_at);
+            done();
+        });
+    });
+
+
+    describe('#game tests', () => {
+        var testGameId;
+        var testGame;
+
+        before('Before add the test game', (done) => {
+            const newGame = new Game();
+            newGame.save((err, game) => {
+                assert(game._id);
+                testGameId = game._id;
+                done(err);
+            });
+        });
+
+        after('After remove the test game', (done) => {
+            Game.findByIdAndRemove(testGameId, (err, game) => {
+                assert(game);
+                done(err);
+            });
+        });
+
+        it('Should retrieve a game by id', (done) => {
+            Game.findById(testGameId, (err, game) => {
+                assert(game);
+                testGame = game;
+                done(err);
+            });
+        });
+
+        it('Should check the game\'s created_at', (done) => {
+            assert(testGame.created_at);
+            done();
+        });
+
+        it('Should start a game', (done) => {
+            assert(!testGame.started_at);
+            testGame.start()
+            assert(testGame.started_at);
+            done();
+        });
+
+        it('Should finish a game', (done) => {
+            assert(!testGame.ended_at);
+            testGame.finish()
+            assert(testGame.ended_at);
+            done();
+        });
+    });
 });
