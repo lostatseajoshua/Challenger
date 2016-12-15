55c71dc0bee736d74523bbd3bbc9accd4595569b
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 10:21:07 -0700

    Remove example test

--- test/test.js
+++ test/test.js
@@ -11,18 +11,6 @@ const assert    = require('assert');
 const request   = require('supertest');
 
 /**
-* Example test.
-*/
-describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-    });
-});
-
-
-/**
 * App route test.
 */
 describe('Express App', () => {
