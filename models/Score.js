a48ce230f7643cc03d28c70698a06ef39abdac2b
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 16:51:34 -0700

    Add extra line spacing

--- models/Score.js
+++ models/Score.js
@@ -3,6 +3,7 @@
  */
 const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
+
 // create a schema
 const ScoreSchema = new Schema({
     team: { type: Schema.Types.ObjectId, required: true},
