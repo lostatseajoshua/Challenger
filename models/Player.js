bacb626efd8ef23f5b70748c7b4972e362edfec9
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 16:51:27 -0700

    Update player model

--- models/Player.js
+++ models/Player.js
@@ -3,24 +3,25 @@
  */
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema
+
 // create a schema
-const playerSchema = new Schema({
+const PlayerSchema = new Schema({
   name: { type: String, minlength: 1 },
   username: { type: String, required: true, unique: true, minlength: 1 },
   team: Schema.Types.ObjectId,
   created_at: Date,
 });
 
-// create a model using schema
-const Player = mongoose.model('Player', playerSchema);
-
 // on every save, add the date
-playerSchema.pre('save', (next) => {
+PlayerSchema.pre('save', function(next) {
   if (!this.created_at) {
     this.created_at = new Date();
   }
   next();
 });
+
+// create a model using schema
+const Player = mongoose.model('Player', PlayerSchema);
 
 // make this available to our users in our Node applications
 module.exports = Player;
