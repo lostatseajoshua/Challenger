0839929b90661c681acfc66b622196202ea7afc8
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 13:33:10 -0700

    Add created_at property in schema

--- models/Game.js
+++ models/Game.js
@@ -6,33 +6,38 @@ const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
 
 // create a schema
-const gameSchema = new Schema({
+const GameSchema = new Schema({
+  created_at: Date,
   started_at: Date,
   ended_at: Date,
   teams: [Schema.Types.ObjectId],
   scores: [Score.schema]
 });
 
-gameSchema.methods.start = function start(completion) {
+// on every save, add the date
+GameSchema.pre('save', function(next) {
+  if (!this.created_at) {
+    this.created_at = new Date();
+  }
+  next();
+});
+
+GameSchema.methods.start = function start() {
   if (!this.started_at) {
     // game is already started
     this.started_at = new Date();
   }
-
-  completion();
 }
 
-gameSchema.methods.finish = function finish(completion) {
+GameSchema.methods.finish = function finish() {
   if (!this.ended_at && this.started_at) {
     // game is already completed
     this.ended_at = new Date();
   }
-
-  completion();
 }
 
 // create a model using schema
-const Game = mongoose.model('Game', gameSchema);
+const Game = mongoose.model('Game', GameSchema);
 
 // make this available to our users in our Node applications
 module.exports = Game;
