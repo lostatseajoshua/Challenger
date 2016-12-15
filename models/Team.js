55f288e61b3f5bee89e83800a16ac5e50388de59
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 10:20:51 -0700

    Replace arrow function to set created at date

--- models/Team.js
+++ models/Team.js
@@ -3,22 +3,23 @@
  */
 const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
+
 // create a schema
-const teamSchema = new Schema({
+const TeamSchema = new Schema({
   name: { type: String, required: true, unique: true, minlength: 1 },
   created_at: Date
 });
 
-// create a model using schema
-const Team = mongoose.model('Team', teamSchema);
-
 // on every save, add the date
-teamSchema.pre('save', (next) => {
+TeamSchema.pre('save', function(next) {
   if (!this.created_at) {
-      this.created_at = new Date();
+    this.created_at = new Date();
   }
   next();
 });
+
+// create a model using schema
+const Team = mongoose.model('Team', TeamSchema);
 
 // make this available to our users in our Node applications
 module.exports = Team;
