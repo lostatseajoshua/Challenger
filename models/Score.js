1d115ff35d71c9cb35b0e8f7fcb345beafc4ed1b
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-15 16:28:23 -0700

    Update score to have created_at property

--- models/Score.js
+++ models/Score.js
@@ -4,13 +4,21 @@
 const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
 // create a schema
-const scoreSchema = new Schema({
+const ScoreSchema = new Schema({
     team: { type: Schema.Types.ObjectId, required: true},
-    points: {type: Number, required: true}
+    points: {type: Number, required: true},
+    created_at: Date
+});
+
+ScoreSchema.pre('save', function(next) {
+    if (!this.created_at) {
+        this.created_at = new Date();
+    }
+    next();
 });
 
 // create a model using schema
-const Score = mongoose.model('Score', scoreSchema);
+const Score = mongoose.model('Score', ScoreSchema);
 
 // make this available to our users in our Node applications
 module.exports = Score;
